6ed6a150b6dfd688cf8f25edbbee8f170208c8ff
daanishvirani <daanishv@gmail.com>
2019-04-10 12:36:50 -0500

    Changed roles for customer

--- src/main/java/springapp/domain/Role.java
+++ src/main/java/springapp/domain/Role.java
@@ -2,6 +2,11 @@ package springapp.domain;
 
 import static springapp.domain.Permission.LIST_CLIENTS;
 import static springapp.domain.Permission.LIST_PETS;
+import static springapp.domain.Permission.GET_CLIENT;
+import static springapp.domain.Permission.GET_PET;
+import static springapp.domain.Permission.SAVE_CLIENT;
+import static springapp.domain.Permission.SAVE_PET;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +15,7 @@ import java.util.List;
 public enum Role {
 
 	SUPER_ADMIN(Permission.values()),
-	READ_ONLY(LIST_PETS, LIST_CLIENTS);
+	CUSTOMER(GET_PET, GET_CLIENT, SAVE_CLIENT, SAVE_PET);
 	
 	
 	private final Permission[] permissions;
